36ca97c372912a1096def85c2d9e85b43cf40c15
wey1998 <yitwey1998@hotmail.com>
2023-04-04 23:18:01 +0100

    Add website logo image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <header className="heading">
       <div className="title">
         <div className="intro-text">
-          <img src="" alt="logo-image" className="logo" />
+          <img src="logo.png" alt="logo-image" className="logo" />
           <h1 className="website-heading">Random Facts about anything</h1>
         </div>
         <button type="button" className="create-task btn">Enter new facts</button>
