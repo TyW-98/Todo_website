2267bd16407926b9f0207dd1802e7359f2b5fd21
wey1998 <yitwey1998@hotmail.com>
2023-04-04 23:16:44 +0100

    Add website heading section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header className="heading">
+      <div className="title">
+        <div className="intro-text">
+          <img src="" alt="logo-image" className="logo" />
+          <h1 className="website-heading">Random Facts about anything</h1>
+        </div>
+        <button type="button" className="create-task btn">Enter new facts</button>
+      </div>
+    </header>
+  );
 }
 
 export default App;
