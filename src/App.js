1cd41bb7ebb7c8df0acf69b7edcdc430f91b8326
wey1998 <yitwey1998@hotmail.com>
2023-04-05 00:18:02 +0100

    Move all components to App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Header from "./Header.js";
+import CategoryFilter from "./CategoryFilter";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <CategoryFilter />
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './Header';
-import CategoryFilter from './CategoryFilter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <CategoryFilter />
+    <App />
   </React.StrictMode>
 );
