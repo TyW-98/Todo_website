5b0407ea6b9b94e062c07572c66837980d36d85b
wey1998 <yitwey1998@hotmail.com>
2023-04-04 01:04:05 +0100

    Use the same for loop to add the source, category and votes for each facts

--- script/script.js
+++ script/script.js
@@ -16,7 +16,7 @@ postBtn.addEventListener("click", function () {
 // Load facts in
 taskList.innerHTML = "";
 
-const initialFacts = [ 
+const initialFacts = [
   {
     id: 1,
     text: "React is being developed by Meta (formerly facebook)",
@@ -32,7 +32,7 @@ const initialFacts = [
     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
     source:
       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
+    category: "social",
     votesInteresting: 11,
     votesMindblowing: 2,
     votesFalse: 0,
@@ -42,7 +42,7 @@ const initialFacts = [
     id: 3,
     text: "Lisbon is the capital of Portugal",
     source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
+    category: "social",
     votesInteresting: 8,
     votesMindblowing: 3,
     votesFalse: 1,
@@ -51,7 +51,18 @@ const initialFacts = [
 ];
 
 for (let i = 0; i < initialFacts.length; i++) {
-    const currentFacts = initialFacts[i].text;
-    const htmlFacts = `<li class="task-item">${currentFacts}</li>`;
-    taskList.insertAdjacentHTML("beforeend",htmlFacts);
-};
+  const currentFacts = initialFacts[i];
+  const htmlFacts = `<li class=task-item>
+    <p>${currentFacts.text}</p>
+    <a href=${currentFacts.source} target="_blank" class="source">(source)</a>
+    <span class="hashtag hashtag-${
+      currentFacts.category === "technology" ? "tech" : "social"
+    }">${currentFacts.category}</span>
+    <div class="emoji-buttons">
+    <button type="button">👍 ${currentFacts.votesInteresting}<strong>1</strong></button>
+    <button type="button">🚀 ${currentFacts.votesMindblowing}<strong>2</strong></button>
+    <button type="button">👎 ${currentFacts.votesFalse}<strong>3</strong></button>
+    </div>
+    </li>`;
+  taskList.insertAdjacentHTML("beforeend", htmlFacts);
+}
