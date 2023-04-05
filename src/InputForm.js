1fe4481f026e5d5664d8edadd6f595a0e46fb798
wey1998 <yitwey1998@hotmail.com>
2023-04-05 19:56:43 +0100

    Add state to inputs in InputForm component

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,17 +1,39 @@
-function InputForm({CATEGORIES}) {
+import { useState } from "react";
+
+function InputForm({ CATEGORIES }) {
+
+  const [text, settext] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+
   return (
     <form>
       <div className="input-form">
-        <input type="text" placeholder="Input your facts here" />
-        <span>200</span>
-        <input type="text" placeholder="Input the URL to the source" />
-        <select>
+        <input
+          type="text"
+          placeholder="Input the fact here"
+          value={text}
+          onChange={(event) => settext(event.target.value)}
+        />
+        <span>{200-text.length}</span>
+        <input
+          type="text"
+          placeholder="Input the URL to the source"
+          value={source}
+          onChange={(event) => setSource(event.target.value)}
+        />
+        <select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
           <option value="">Choose a category</option>
           {CATEGORIES.map((cat) => (
-            <option value={cat.name}>{cat.name}</option>
+            <option value={cat.name}>{cat.name.toUpperCase()}</option>
           ))}
         </select>
-        <button type="button" className="btn btn-post">Post fact</button>
+        <button className="btn btn-post">
+          Post fact
+        </button>
       </div>
     </form>
   );
