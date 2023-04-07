1e847888219b1db8f8f2cd5fb0a4eb6a48644935
wey1998 <yitwey1998@hotmail.com>
2023-04-07 02:01:43 +0100

    Add all voting functionality to all emoji voting buttons

--- src/Voting.js
+++ src/Voting.js
@@ -1,31 +1,36 @@
 import supabase from "./db.js";
+import { useState } from "react";
 
 function Voting(props) {
 
   const fact = props.fact;
   const setFacts = props.setFacts;
+  const [updatingBtn, setUpdatingBtn] = useState(false);
 
-  async function handleVotes(btnName) {
+  async function handleVotes(selectedBtn) {
+    setUpdatingBtn(true);
     const { data: updatedFact, error } = await supabase
       .from("Facts")
-      .update({ votesInteresting: fact.votesInteresting + 1 })
+      .update({ [selectedBtn]: fact[selectedBtn] + 1 })
       .eq("id", fact.id)
       .select();
 
+    setUpdatingBtn(false);
+
       if (!error) {
         setFacts((facts) => facts.map ((f) => f.id === fact.id ? updatedFact[0] : f))
-      }
+      };
   }
 
   return (
     <>
-      <button type="button" onClick={handleVotes}>
+      <button type="button" onClick={() => handleVotes("votesInteresting")} disabled={updatingBtn}>
         👍 <strong>{fact.votesInteresting}</strong>
       </button>
-      <button type="button">
+      <button type="button" onClick={() => handleVotes("votesMindblowing")} disabled={updatingBtn}>
         🚀 <strong>{fact.votesMindblowing}</strong>
       </button>
-      <button type="button">
+      <button type="button" onClick={() => handleVotes("votesFalse")} disabled={updatingBtn}>
         👎 <strong>{fact.votesFalse}</strong>
       </button>
     </>
